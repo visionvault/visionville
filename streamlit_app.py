4646064cd8ab851d4ee7ef8943fa796c7a8cb404
visionvault <140893010+visionvault@users.noreply.github.com>
2023-10-30 10:26:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as st
+st.title ("Visionville")
+st.header("Newsfeed app")
+st.markdown("Different components")
+st.subheader("Lite")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 st.write('Hello world!')
